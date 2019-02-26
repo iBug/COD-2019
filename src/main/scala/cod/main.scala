841b83d17c8a65fe90f03e7950cf5f59efb76cbd
iBug <7273074+iBug@users.noreply.github.com>
2019-02-26 20:02:12 +0800

    Default FIRRTL and Verilog output to target/

--- src/main/scala/cod/main.scala
+++ src/main/scala/cod/main.scala
@@ -4,5 +4,9 @@ import chisel3._
 import chisel3.util._
 
 object Main extends App {
-  // chisel3.Driver.execute(args, () => new DumbCore)
+  var new_args = args
+  if (!(args contains "--target")) {
+    new_args ++= Array("--target", "target")
+  }
+  // chisel3.Driver.execute(new_args, () => new DumbCore)
 }
