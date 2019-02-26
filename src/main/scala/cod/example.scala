74e43de0dae122c828e37cddf538b56cd706278c
iBug <7273074+iBug@users.noreply.github.com>
2019-02-26 23:28:25 +0800

    Add dummy module and Scalastyle linter
    Linter is great

---
+++ src/main/scala/cod/example.scala
@@ -0,0 +1,13 @@
+package cod
+
+import chisel3._
+import chisel3.util._
+
+class Example extends Module {
+  val io = IO(new Bundle{
+    val x = Input(UInt(8.W))
+    val y = Output(UInt(4.W))
+  })
+  
+  io.y := Cat(io.x(7), io.x(5), io.x(3), io.x(1))
+}
--- src/main/scala/cod/main.scala
+++ src/main/scala/cod/main.scala
@@ -5,8 +5,9 @@ import chisel3.util._
 
 object Main extends App {
   var new_args = args
-  if (!(args contains "--target")) {
-    new_args ++= Array("--target", "target")
+  if (!((args contains "--target-dir") || (args contains "-td"))) {
+    new_args ++= Array("--target-dir", "target")
   }
-  // chisel3.Driver.execute(new_args, () => new DumbCore)
+
+  chisel3.Driver.execute(new_args, () => new Example)
 }
