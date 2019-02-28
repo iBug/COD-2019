371848a4f34a10d51ea53f67295e76d9842efdd2
iBug <iBug+7273074@users.noreply.github.com>
2019-02-28 11:46:28 +0800

    minor fixes

--- src/main/scala/cod/Main.scala
+++ src/main/scala/cod/Main.scala
@@ -4,10 +4,10 @@ import chisel3._
 import chisel3.util._
 
 object Main extends App {
-  var new_args = args
-  if (!((args contains "--target-dir") || (args contains "-td"))) {
-    new_args ++= Array("--target-dir", "target")
+  var Args: Array[String] = args
+  if (!((Args contains "--target-dir") || (Args contains "-td"))) {
+    Args ++= Array("--target-dir", "target")
   }
 
-  chisel3.Driver.execute(new_args, () => new Adder(32))
+  chisel3.Driver.execute(Args, () => new Adder(32))
 }
