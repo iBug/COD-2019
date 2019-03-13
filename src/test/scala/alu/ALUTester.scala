00e37d2a0bba91e6ebda78f553485e52ae2b8f0a
iBug <7273074+iBug@users.noreply.github.com>
2019-03-13 20:49:33 +0800

    Work around bad Java signed integers

--- src/test/scala/alu/ALUTester.scala
+++ src/test/scala/alu/ALUTester.scala
@@ -10,19 +10,19 @@ class AndTester(c: And) extends PeekPokeTester(c) {
   poke(c.io.A, 0x12345678)
   poke(c.io.B, 0xFEDCBA90)
   step(1)
-  expect(c.io.S, 0x12141210)
+  expect(c.io.S, 0x12141210L)
 }
 
 class OrTester(c: Or) extends PeekPokeTester(c) {
   poke(c.io.A, 0x12345678)
   poke(c.io.B, 0xFEDCBA90)
   step(1)
-  expect(c.io.S, 0xFEFCFEF8)
+  expect(c.io.S, 0xFEFCFEF8L)
 }
 
 class XorTester(c: Xor) extends PeekPokeTester(c) {
   poke(c.io.A, 0x12345678)
   poke(c.io.B, 0xFEDCBA90)
   step(1)
-  expect(c.io.S, 0xECE8ECE8)
+  expect(c.io.S, 0xECE8ECE8L)
 }
--- src/test/scala/alu/AdderTester.scala
+++ src/test/scala/alu/AdderTester.scala
@@ -2,9 +2,9 @@ package alu
 
 import chisel3._
 import chisel3.util._
-import chisel3.iotesters
 import chisel3.iotesters._
 import org.scalatest.{FlatSpec, Matchers}
+import scala.util.Random
 
 class AdderTester(c: Adder) extends PeekPokeTester(c) {
   poke(c.io.A, 0x1)
@@ -20,4 +20,18 @@ class AdderTester(c: Adder) extends PeekPokeTester(c) {
   step(1)
   expect(c.io.Sum, 0x1)
   expect(c.io.Cout, 1)
+
+  val r = scala.util.Random
+  for (n <- 0 until 4) {
+    val a = math.abs(r.nextInt).toLong
+    val b = math.abs(r.nextInt).toLong
+    val cin = r.nextInt(2)
+    val sum = a + b + cin
+    poke(c.io.A, a)
+    poke(c.io.B, b)
+    poke(c.io.Cin, cin)
+    step(1)
+    expect(c.io.Sum, (sum + 4294967296L) % 4294967296L)
+    expect(c.io.Cout, sum >= (4294967296L))
+  }
 }
