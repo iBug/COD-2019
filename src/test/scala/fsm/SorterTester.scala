5f0bf8cf9d8cf7bb8b2d7cfc1da5546529016339
iBug <7273074+iBug@users.noreply.github.com>
2019-03-24 13:08:14 +0800

    Randomly generate tests for Sorter4

--- src/test/scala/fsm/SorterTester.scala
+++ src/test/scala/fsm/SorterTester.scala
@@ -4,28 +4,35 @@ import chisel3._
 import chisel3.util._
 import chisel3.iotesters._
 import org.scalatest.{FlatSpec, Matchers}
+import scala.util.Random
 
 class SorterTester(val c: Sorter4) extends PeekPokeTester(c) {
   val d = c.io
-  poke(d.x0, 8)
-  poke(d.x1, 3)
-  poke(d.x2, 2)
-  poke(d.x3, 5)
+  val r = scala.util.Random
 
-  reset(1)
-  expect(d.s0, 8)
-  expect(d.s1, 3)
-  expect(d.s2, 2)
-  expect(d.s3, 5)
-  expect(d.done, false)
+  for (n <- 0 until 10) {
+    val x = List.tabulate(4)(e => r.nextInt(100))
+    poke(d.x0, x(0))
+    poke(d.x1, x(1))
+    poke(d.x2, x(2))
+    poke(d.x3, x(3))
 
-  step(2)
-  expect(d.done, false)
+    reset(1)
+    expect(d.s0, x(0))
+    expect(d.s1, x(1))
+    expect(d.s2, x(2))
+    expect(d.s3, x(3))
+    expect(d.done, false)
 
-  step(2)
-  expect(d.done, true)
-  expect(d.s0, 2)
-  expect(d.s1, 3)
-  expect(d.s2, 5)
-  expect(d.s3, 8)
+    step(2)
+    expect(d.done, false)
+
+    val s = x.sorted(Ordering.Int)
+    step(1)
+    expect(d.done, true)
+    expect(d.s0, s(0))
+    expect(d.s1, s(1))
+    expect(d.s2, s(2))
+    expect(d.s3, s(3))
+  }
 }
