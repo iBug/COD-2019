8748d8da94617b7256daa6f4083eb50ff9bbe8aa
iBug <7273074+iBug@users.noreply.github.com>
2019-03-24 13:15:19 +0800

    Also use random numbers on Divisor

--- src/test/scala/fsm/DivisorTester.scala
+++ src/test/scala/fsm/DivisorTester.scala
@@ -4,31 +4,36 @@ import chisel3._
 import chisel3.util._
 import chisel3.iotesters._
 import org.scalatest.{FlatSpec, Matchers}
+import scala.util.Random
 
 class DivisorTester(val c: Divisor) extends PeekPokeTester(c) {
   val d = c.io
-  poke(d.x, 71)
-  poke(d.y, 7)
+  val r = Random
 
-  reset(1)
-  expect(d.error, false)
-  step(c.w)
-  expect(d.q, 10)
-  expect(d.r, 1)
-  expect(d.done, true)
+  for (n <- 0 until 10) {
+    val x = r.nextInt(101)
+    val y = r.nextInt(11)
+    poke(d.x, x)
+    poke(d.y, y)
 
-  poke(d.x, 107)
-  poke(d.y, 9)
-
-  reset(1)
-  expect(d.error, false)
-  step(c.w)
-  expect(d.q, 11)
-  expect(d.r, 8)
-  expect(d.done, true)
+    reset(1)
+    expect(d.q, 0)
+    expect(d.r, 0)
+    expect(d.error, false)
+    step(c.w)
+    expect(d.q, x / y)
+    expect(d.r, x % y)
+    expect(d.done, true)
+  }
 
   // Test error
   poke(d.y, 0)
   reset(1)
+  expect(d.q, 0)
+  expect(d.r, 0)
+  expect(d.error, true)
+  step(1)
+  expect(d.q, 0)
+  expect(d.r, 0)
   expect(d.error, true)
 }
