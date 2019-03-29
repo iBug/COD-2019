369f4d23e2504167da0f68cf1290850193e8975c
iBug <7273074+iBug@users.noreply.github.com>
2019-03-29 21:33:16 +0800

    Implement Counter.scala

---
+++ src/main/scala/reg/Counter.scala
@@ -0,0 +1,22 @@
+package reg
+
+import chisel3._
+import chisel3.util._
+
+class Counter(val w: Int) extends Module {
+  val io = IO(new Bundle {
+    val ce = Input(Bool())
+    val pe = Input(Bool())
+    val d = Input(UInt(w.W))
+    val q = Output(UInt(w.W))
+  })
+
+  val q = RegInit(0.U(w.W))
+  io.q := q
+
+  when (pe) {
+    q := io.d
+  } .elsewhen (ce) {
+    q := q + 1.U
+  }
+}
