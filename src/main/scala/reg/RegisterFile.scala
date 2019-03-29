adc0da7894eb82853042e144ec5341e1ae721bbf
iBug <7273074+iBug@users.noreply.github.com>
2019-03-29 22:09:50 +0800

    Rename aWidth -> wAddr

--- src/main/scala/reg/RegisterFile.scala
+++ src/main/scala/reg/RegisterFile.scala
@@ -3,18 +3,18 @@ package reg
 import chisel3._
 import chisel3.util._
 
-class RegisterFile(val aWidth: Int, val dWidth: Int) extends Module {
+class RegisterFile(val wData: Int, val wAddr: Int) extends Module {
   val io = IO(new Bundle {
-    val ra0 = Input(UInt(aWidth.W))
-    val ra1 = Input(UInt(aWidth.W))
-    val rd0 = Output(UInt(dWidth.W))
-    val rd1 = Output(UInt(dWidth.W))
-    val wa0 = Input(UInt(aWidth.W))
-    val wd0 = Input(UInt(dWidth.W))
+    val ra0 = Input(UInt(wAddr.W))
+    val ra1 = Input(UInt(wAddr.W))
+    val rd0 = Output(UInt(wData.W))
+    val rd1 = Output(UInt(wData.W))
+    val wa0 = Input(UInt(wAddr.W))
+    val wd0 = Input(UInt(wData.W))
     val we = Input(Bool())
   })
 
-  val r = RegInit(VecInit(Seq.fill(1 << aWidth)(0.U(dWidth.W))))
+  val r = RegInit(VecInit(Seq.fill(1 << wAddr)(0.U(wData.W))))
 
   io.rd0 := r(io.ra0)
   io.rd1 := r(io.ra1)
