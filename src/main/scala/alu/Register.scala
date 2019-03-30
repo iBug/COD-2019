4921f7f0cd1d78334e8d93d52dafda961e4b1104
iBug <7273074+iBug@users.noreply.github.com>
2019-03-30 23:40:31 +0800

    Add default (reset) value for alu.Register

--- src/main/scala/alu/Register.scala
+++ src/main/scala/alu/Register.scala
@@ -4,22 +4,22 @@ import chisel3._
 import chisel3.util._
 
 object Register {
-  def apply(w: Int, in: UInt, enable: Bool): UInt = {
-    val m = Module(new Register(w)).io
+  def apply(w: Int, in: UInt, enable: Bool, init: Int = 0): UInt = {
+    val m = Module(new Register(w, init)).io
     m.in := in
     m.enable := enable
     m.out
   }
 }
 
-class Register(w: Int) extends Module {
+class Register(val w: Int, val init: Int = 0) extends Module {
   val io = IO(new Bundle {
     val in = Input(UInt(w.W))
     val out = Output(UInt(w.W))
     val enable = Input(Bool())
   })
 
-  val reg = RegInit(0.U(w.W))
+  val reg = RegInit(init.U(w.W))
   io.out := reg
 
   when (io.enable) {
