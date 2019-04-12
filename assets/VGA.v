3ffb0be801d1a07ad0f20ed6143a4f62748f044f
iBug <7273074+iBug@users.noreply.github.com>
2019-04-12 23:05:50 +0800

    Switch to 100 MHz clock, built-in clock divider

--- assets/VGA.v
+++ assets/VGA.v
@@ -1,4 +1,5 @@
 module VGA (
+    // Clock requirement: 100 MHz
     input clk, rst,
     output hs, vs,
     output en,
@@ -7,6 +8,8 @@ module VGA (
     parameter HD = 640, HF = 16, HS = 96, HB = 48;
     parameter VD = 480, VF = 10, VS = 2, VB = 31;
 
+    reg ce;
+    reg [15:0] count;
     reg [15:0] hc, vc;
     assign x = en ? hc : 0;
     assign y = en ? vc : 0;
@@ -14,12 +17,20 @@ module VGA (
     assign hs = ~((hc >= HD + HF) && (hc < HD + HF + HS));
     assign vs = ~((vc >= VD + VF) && (vc < VD + VF + VS));
 
-    // Clock requirement: 25 MHz
+    // Clock divider
+    always @(posedge clk) begin
+        if (rst)
+            {ce, count} <= 0;
+        else
+            {ce, count} <= count + 16'h4000;
+    end
+
+    // VGA Scanner
     always @(posedge clk) begin
         if (rst) begin
             hc <= 0;
             vc <= 0;
-        end else begin
+        end else if (ce) begin
             if (hc >= HD + HF + HS + HB - 1)
                 hc <= 0;
             else begin
